8d8f650ae18e9e5fe27ba97f5b78bb75312aabf0
João Victor Reis Alves <joao.reisa13@gmail.com>
2022-02-17 18:35:08 -0300

    Change the default color to deepPurple

--- lib/config/text_styles.dart
+++ lib/config/text_styles.dart
@@ -16,6 +16,6 @@ class TextStyles {
   static final subtitleTextStyle =
       standardTextStyle.copyWith(color: Colors.grey[700], fontSize: 20);
 
-  static final textButtonStyles =
-      standardTextStyle.copyWith(color: Colors.deepPurple);
+  static final textButtonStyles = standardTextStyle.copyWith(
+      color: Colors.deepPurple, fontWeight: FontWeight.bold);
 }
