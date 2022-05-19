9dbdb04a3a699e9c77a834fe4c4d17edb70b83ae
João Victor Reis Alves - JAV <joao.alves@iteris.com.br>
2022-05-19 16:02:49 -0300

    build script

--- deploy_web_build.sh
+++ deploy_web_build.sh
@@ -4,6 +4,12 @@ flutter clean && flutter pub get
 flutter build web
 
 cd build/web
-git add -A
+git init
+git remote add origin https://github.com/JoaoReisA/JoaoReisA.github.io.git
+git add .
+git branch -M main
 git commit -m "Deploy web build"
+git pull origin main --rebase
+git add .
+git rebase --continue
 git push origin main
